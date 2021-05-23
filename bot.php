c615d05870417bc1c73d050510f1cd7ac087b3c1
Yhya syrian <77106078+yhyasyrian@users.noreply.github.com>
2021-05-23 16:57:14 +0300

    Add files via upload

---
+++ bot.php
@@ -0,0 +1,77 @@
+<?php
+/* 
+ * Telegram Channel @TeamSyria 🍃
+  * Telegram Channel @HLOOFiles 💯
+  * Telegram MyUser @KKYKKN 💕
+*/
+error_reporting(1);
+$Token = '';// Your Token
+$Admin = '';// Your Id
+define('API_KEY',$Token);
+function bot($method,$datas=[]){
+    $url = "https://api.telegram.org/bot".API_KEY."/".$method;
+$ch = curl_init();
+    curl_setopt($ch,CURLOPT_URL,$url);
+    curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
+    curl_setopt($ch,CURLOPT_POSTFIELDS,$datas);
+    $res = curl_exec($ch);
+    if(curl_error($ch)){
+        var_dump(curl_error($ch));
+    }else{
+        return json_decode($res);
+    }
+}
+bot('deletewebhook',[]);
+function getupdate($offset){
+	return bot('getupdates',[
+	'offset'=>$offset,
+	])->result[0];
+}
+function run($update){
+print_r($update);
+$admin = $GLOBALS['Admin'];
+$message = $update->message;
+$message_id = $message->message_id;
+$chat_id = $message->chat->id;
+$text = $message->text;
+$user = $message->from->username;
+$name = $message->from->first_name;
+$from_id = $message->from->id;
+$type = $message->chat->type;
+if($text == "/start" and $type == "private"){
+bot('sendmessage',[
+'chat_id'=>$chat_id,
+'text'=>"I Am Work 😏💯"
+]);
+}
+if($text == "/stop" and $type == "private"){
+bot('sendmessage',[
+'chat_id'=>$chat_id,
+'text'=>"Ok 💔"
+]);
+return 'stop';
+}
+}
+while(true){
+try{
+	$update_id = $update_id ?? 0;
+	$update = getupdate($update_id+1);
+	$ok = run($update);
+	$update_id = $update->update_id;
+	if($ok == 'stop'){
+		$update = getupdate($update_id+2);
+	  	break;
+	  }
+} catch (Exception $e) {
+bot('sendmessage',[
+'chat_id'=>$admin,
+'text'=>$e->getMessage(),
+]);
+break;
+}
+}
+/* 
+ * Telegram Channel @TeamSyria 🍃
+  * Telegram Channel @HLOOFiles 💯
+  * Telegram MyUser @KKYKKN 💕
+*/
